a04768318ed5628e6481f28c00b51f53de7b7d84
Steve Streeting <steve@stevestreeting.com>
2024-02-29 17:35:31 +0000

    Initial plugin creation

---
+++ Source/SUSS/Private/SUSS.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SUSS.h"
+#include "Misc/MessageDialog.h"
+#include "Modules/ModuleManager.h"
+#include "Misc/Paths.h"
+#include "HAL/PlatformProcess.h"
+
+#define LOCTEXT_NAMESPACE "FSUSSModule"
+
+void FSUSSModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FSUSSModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FSUSSModule, SUSS)
---
+++ Source/SUSS/Public/SUSS.h
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FSUSSModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+	
+};
