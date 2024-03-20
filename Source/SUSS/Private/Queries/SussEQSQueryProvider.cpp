0c37ee740982baa64b39ee213dddf3778d7417f6
Steve Streeting <steve@stevestreeting.com>
2024-03-20 18:06:27 +0000

    Add base class for using EQS as a backend for queries

---
+++ Source/SUSS/Private/Queries/SussEQSQueryProvider.cpp
@@ -0,0 +1,108 @@
+﻿
+#include "Queries/SussEQSQueryProvider.h"
+
+#include "EnvironmentQuery/EnvQueryManager.h"
+#include "Misc/TransactionObjectEvent.h"
+
+void USussEQSQueryProvider::RunEQSQuery(USussBrainComponent* Brain,
+                                        AActor* Self,
+                                        const TMap<FName, FSussParameter>& Params,
+                                        TFunctionRef<void(const FEnvQueryItem& Result)> ResultCallback)
+{
+	if (!EQSQuery)
+		return;
+
+	if (UEnvQueryManager* EQS = UEnvQueryManager::GetCurrent(Self->GetWorld()))
+	{
+		// We *could* allow EQS to be run in steps over many frames here
+		// See ExecuteOneStep inside this function, or EQSTestingPawn
+		// Or we could use RunQuery with callback.
+		// For now, run synchronous for simplicity, and limit time between AIs
+		FEnvQueryRequest QueryRequest(EQSQuery, this);
+		for (FAIDynamicParam& Param : QueryConfig)
+		{
+			QueryRequest.SetDynamicParam(Param);
+		}
+		// Also set params from incoming instance request
+		for (const auto& Param : Params)
+		{
+			// Only set params which are registered to this query, so shared params can be used
+			if (ParamNames.Contains(Param.Key))
+			{
+				const FSussParameter& InParam = Param.Value;
+				FAIDynamicParam DParam;
+				DParam.ParamName = Param.Key;
+				switch (Param.Value.Type)
+				{
+				case ESussParamType::Float:
+					DParam.ParamType = EAIParamType::Float;
+					DParam.Value = InParam.FloatValue;
+					break;
+				case ESussParamType::Int:
+					DParam.ParamType = EAIParamType::Int;
+					DParam.Value = InParam.IntValue;
+					break;
+				case ESussParamType::Tag:
+				case ESussParamType::Input:
+					// Not supported
+					continue;
+				}
+				QueryRequest.SetDynamicParam(DParam);
+			}
+		}
+		auto Result = EQS->RunInstantQuery(QueryRequest, QueryMode);
+
+		if (CheckResultClass(Result->ItemType))
+		{
+			for (const FEnvQueryItem& Item : Result->Items)
+			{
+				ResultCallback(Item);
+			}
+		}
+	}
+}
+
+
+#if WITH_EDITOR
+void USussEQSQueryProvider::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	if (PropertyChangedEvent.Property != nullptr)
+	{
+		OnPropertyChanged(PropertyChangedEvent.MemberProperty->GetFName());
+	}
+
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+void USussEQSQueryProvider::PostTransacted(const FTransactionObjectEvent& TransactionEvent)
+{
+	Super::PostTransacted(TransactionEvent);
+
+	if (TransactionEvent.GetEventType() == ETransactionObjectEventType::UndoRedo)
+	{
+		if (TransactionEvent.GetChangedProperties().Num() > 0)
+		{
+			// targeted update
+			for (const FName PropertyName : TransactionEvent.GetChangedProperties())
+			{
+				OnPropertyChanged(PropertyName);
+			}
+		}
+	}
+}
+
+void USussEQSQueryProvider::OnPropertyChanged(const FName PropName)
+{
+	// This is to update our parameter list based on the query selected
+	static const FName NAME_Query = GET_MEMBER_NAME_CHECKED(USussEQSQueryProvider, EQSQuery);
+	static const FName NAME_QueryConfig = GET_MEMBER_NAME_CHECKED(USussEQSQueryProvider, QueryConfig);
+
+	if (PropName == NAME_Query || PropName == NAME_QueryConfig)
+	{
+		if (EQSQuery)
+		{
+			EQSQuery->CollectQueryParams(*this, QueryConfig);
+		}
+	}
+}
+#endif // WITH_EDITOR
---
+++ Source/SUSS/Public/Queries/SussEQSQueryProvider.h
@@ -0,0 +1,45 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SussQueryProvider.h"
+#include "EnvironmentQuery/EnvQuery.h"
+#include "SussEQSQueryProvider.generated.h"
+
+/**
+ * This query provider delegates to an EQS query. Use one of the subclasses for specific result types (e.g. Location)
+ */
+UCLASS(Abstract)
+class SUSS_API USussEQSQueryProvider : public USussQueryProvider
+{
+	GENERATED_BODY()
+protected:
+	/// Link to the EQS query which this provider will run
+	UPROPERTY(EditDefaultsOnly, Category=Query)
+	UEnvQuery* EQSQuery;
+
+	/// How many results we're looking for from this query
+	UPROPERTY(EditDefaultsOnly, Category=Query)
+	TEnumAsByte<EEnvQueryRunMode::Type> QueryMode = EEnvQueryRunMode::AllMatching;
+
+	/// EQS parameter values which are static. Note that instance params can still be supplied when this query is used
+	UPROPERTY(EditDefaultsOnly, Category=Query)
+	TArray<FAIDynamicParam> QueryConfig;
+
+
+public:
+	
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void PostTransacted(const FTransactionObjectEvent& TransactionEvent) override;
+	void OnPropertyChanged(const FName PropName);
+#endif // WITH_EDITOR
+
+protected:
+	bool CheckResultClass(TSubclassOf<UEnvQueryItemType> ResultClass) { return false; } // needs to be overridden
+	void RunEQSQuery(USussBrainComponent* Brain,
+	                 AActor* Self,
+	                 const TMap<FName, FSussParameter>& Params,
+	                 TFunctionRef<void(const FEnvQueryItem& Result)> ResultCallback);
+};
