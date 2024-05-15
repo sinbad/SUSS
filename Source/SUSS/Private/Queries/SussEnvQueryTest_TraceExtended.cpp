035f44ffb3d418de2831063f8543faac2858283b
Steve Streeting <steve@stevestreeting.com>
2024-05-15 17:57:43 +0100

    Add a "Trace Extended" EQS test
    Main benefit is you can add start & end offsets along the trace direction, avoiding false positives with close objects & wide trace widths

---
+++ Source/SUSS/Private/Queries/SussEnvQueryTest_TraceExtended.cpp
@@ -0,0 +1,93 @@
+﻿
+#include "Queries/SussEnvQueryTest_TraceExtended.h"
+
+void USussEnvQueryTest_TraceExtended::RunTest(FEnvQueryInstance& QueryInstance) const
+{
+	// Copied from superclass since we can't call it
+	UObject* DataOwner = QueryInstance.Owner.Get();
+	BoolValue.BindData(DataOwner, QueryInstance.QueryID);
+	TraceFromContext.BindData(DataOwner, QueryInstance.QueryID);
+	ItemHeightOffset.BindData(DataOwner, QueryInstance.QueryID);
+	ContextHeightOffset.BindData(DataOwner, QueryInstance.QueryID);
+	
+	bool bWantsHit = BoolValue.GetValue();
+	bool bTraceToItem = TraceFromContext.GetValue();
+	float ItemZ = ItemHeightOffset.GetValue();
+	float ContextZ = ContextHeightOffset.GetValue();
+
+	// SUSS begin
+	ContextTraceOffset.BindData(DataOwner, QueryInstance.QueryID);
+	ItemTraceOffset.BindData(DataOwner, QueryInstance.QueryID);
+	const float ContextOffsetVal = ContextTraceOffset.GetValue();
+	const float ItemOffsetVal = ItemTraceOffset.GetValue();
+	// SUSS end
+
+	TArray<FVector> ContextLocations;
+	if (!QueryInstance.PrepareContext(Context, ContextLocations))
+	{
+		return;
+	}
+
+	FCollisionQueryParams TraceParams(SCENE_QUERY_STAT(EnvQueryTrace), TraceData.bTraceComplex);
+
+	TArray<AActor*> IgnoredActors;
+	if (QueryInstance.PrepareContext(Context, IgnoredActors))
+	{
+		TraceParams.AddIgnoredActors(IgnoredActors);
+	}
+	
+	ECollisionChannel TraceCollisionChannel = UEngineTypes::ConvertToCollisionChannel(TraceData.TraceChannel);	
+	FVector TraceExtent(TraceData.ExtentX, TraceData.ExtentY, TraceData.ExtentZ);
+	FRunTraceSignature TraceFunc;
+	switch (TraceData.TraceShape)
+	{
+	case EEnvTraceShape::Line:
+		TraceFunc.BindUObject(const_cast<USussEnvQueryTest_TraceExtended*>(this), bTraceToItem ? &USussEnvQueryTest_TraceExtended::RunLineTraceTo : &USussEnvQueryTest_TraceExtended::RunLineTraceFrom);
+		break;
+
+	case EEnvTraceShape::Box:
+		TraceFunc.BindUObject(const_cast<USussEnvQueryTest_TraceExtended*>(this), bTraceToItem ? &USussEnvQueryTest_TraceExtended::RunBoxTraceTo : &USussEnvQueryTest_TraceExtended::RunBoxTraceFrom);
+		break;
+
+	case EEnvTraceShape::Sphere:
+		TraceFunc.BindUObject(const_cast<USussEnvQueryTest_TraceExtended*>(this), bTraceToItem ? &USussEnvQueryTest_TraceExtended::RunSphereTraceTo : &USussEnvQueryTest_TraceExtended::RunSphereTraceFrom);
+		break;
+
+	case EEnvTraceShape::Capsule:
+		TraceFunc.BindUObject(const_cast<USussEnvQueryTest_TraceExtended*>(this), bTraceToItem ? &USussEnvQueryTest_TraceExtended::RunCapsuleTraceTo : &USussEnvQueryTest_TraceExtended::RunCapsuleTraceFrom);
+		break;
+
+	default:
+		return;
+	}
+
+	for (int32 ContextIndex = 0; ContextIndex < ContextLocations.Num(); ContextIndex++)
+	{
+		ContextLocations[ContextIndex].Z += ContextZ;
+	}
+
+	for (FEnvQueryInstance::ItemIterator It(this, QueryInstance); It; ++It)
+	{
+		// SUSS begin
+		FVector ItemLocation = GetItemLocation(QueryInstance, It.GetIndex()) + FVector(0, 0, ItemZ);
+		AActor* ItemActor = GetItemActor(QueryInstance, It.GetIndex());
+
+		for (int32 ContextIndex = 0; ContextIndex < ContextLocations.Num(); ContextIndex++)
+		{
+			FVector ContextLocation = ContextLocations[ContextIndex];
+			// Offset the ends if needed
+			if (!FMath::IsNearlyZero(ContextOffsetVal) || !FMath::IsNearlyZero(ItemOffsetVal))
+			{
+				FVector ContextToItemVec =  ItemLocation - ContextLocation;
+				ContextToItemVec.Normalize();
+				ContextLocation += ContextOffsetVal * ContextToItemVec;
+				ItemLocation -= ItemOffsetVal * ContextToItemVec;
+			}
+
+			// SUSS end
+			
+			const bool bHit = TraceFunc.Execute(ItemLocation, ContextLocation, ItemActor, QueryInstance.World, TraceCollisionChannel, TraceParams, TraceExtent);
+			It.SetScore(TestPurpose, FilterType, bHit, bWantsHit);
+		}
+	}
+}
---
+++ Source/SUSS/Public/Queries/SussEnvQueryTest_TraceExtended.h
@@ -0,0 +1,29 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/Tests/EnvQueryTest_Trace.h"
+#include "SussEnvQueryTest_TraceExtended.generated.h"
+
+/**
+ * Enhanced version of UEnvQueryTest_Trace which allows you to start / end the trace at an offset from
+ * the item / context. This can be very useful when you want a wide trace, but this would cause it to return
+ * false positives if traced all the way to the item / context, because it could clip things on the other side.
+ */
+UCLASS()
+class SUSS_API USussEnvQueryTest_TraceExtended : public UEnvQueryTest_Trace
+{
+	GENERATED_BODY()
+
+	/// Offset along the line of the trace to start/end near the item
+	UPROPERTY(EditDefaultsOnly, Category=Trace, AdvancedDisplay)
+	FAIDataProviderFloatValue ItemTraceOffset;
+
+	/// Offset along the line of the trace to start/end near the item
+	UPROPERTY(EditDefaultsOnly, Category=Trace, AdvancedDisplay)
+	FAIDataProviderFloatValue ContextTraceOffset;
+
+public:
+	virtual void RunTest(FEnvQueryInstance& QueryInstance) const override;
+};
